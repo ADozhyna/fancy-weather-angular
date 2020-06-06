ec9ae8a36276d19bb3aad83bb5a8f0b7c4cfa990
hanna dozhyna <annadozhyna@gmail.com>
2020-06-06 20:55:38 +0300

    fix: basic structure

--- fancy-weather/src/app/app.module.ts
+++ fancy-weather/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { CoreModule } from './src/app/core/core.module';
+import { CoreModule } from './core/core.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
